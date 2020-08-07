5f10e5749b1dc0dbfb54078f2e4a3a7b9a39447f
Alexander Ho <alexanderho1998@gmail.com>
2020-08-07 13:03:37 -0500

    Added Contact Page title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactPage } from './contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactPage
   ],
   imports: [
     BrowserModule,
---
+++ src/app/contact-page/contact-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-contact-page',
+    templateUrl: './contact-page.component.html',
+    styleUrls: ['contact-page.component.css']
+})
+
+export class ContactPage {
+
+}
