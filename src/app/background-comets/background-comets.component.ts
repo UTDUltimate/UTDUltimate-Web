e91efa76da958fde8bd5e5137de37638a72a7860
Jamie Eriksson <jamieeriksson@gmail.com>
2020-08-12 17:20:52 -0700

    Change comet positions to fixed

--- src/app/background-comets/background-comets.component.ts
+++ src/app/background-comets/background-comets.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-background-comets',
-  templateUrl: './background-comets.component.html',
-  styleUrls: ['./background-comets.component.css']
+  selector: "app-background-comets",
+  templateUrl: "./background-comets.component.html",
+  styleUrls: ["./background-comets.component.css"],
 })
 export class BackgroundCometsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
