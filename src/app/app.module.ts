50bc242ec333623c6ec582108f37f9a1f9272af2
Karatekk2 <johnnovosel96@gmail.com>
2020-08-24 12:51:18 -0500

    fixed merge conflicts from contactus pull

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
@@ -13,18 +12,6 @@ import { InMemoryDataService } from "./services/in-memory-data.service";
 import { RosterListComponent } from "./roster-page/roster-list/roster-list.component";
 import { BackgroundCometsComponent } from './background-comets/background-comets.component';
 import {NavbarComponent} from './navbar/navbar.component';
-=======
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './Home/home.component';
-import { AboutPageComponent } from './about-page/about-page.component';
-import { FooterComponent } from './footer/footer.component';
-import { ContactUsComponent } from './contact-us/contact-us.component';
-
->>>>>>> 81be0ea0a6c17a2b21f5c5d04b059a0ba6a8d897
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatExpansionModule} from '@angular/material/expansion';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
@@ -37,13 +24,9 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
     HomeComponent,
     AboutPageComponent,
     FooterComponent,
-<<<<<<< HEAD
     NavbarComponent,
     RosterListComponent,
     BackgroundCometsComponent,
-=======
-    ContactUsComponent
->>>>>>> 81be0ea0a6c17a2b21f5c5d04b059a0ba6a8d897
   ],
   imports: [
     BrowserModule,
