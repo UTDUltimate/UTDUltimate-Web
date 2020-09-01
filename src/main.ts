6c09fc6fe07e6fcd3eed5d6a1ca308f8c5825283
Alexander Ho <alexanderho1998@gmail.com>
2020-08-31 20:43:45 -0500

    Added gallery-page.component

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 import Amplify from "aws-amplify";
-import aws_exports from "./aws-exports";
-Amplify.configure(aws_exports);
+// import aws_exports from "./aws-exports";
+// Amplify.configure(aws_exports);
 
 if (environment.production) {
   enableProdMode();
