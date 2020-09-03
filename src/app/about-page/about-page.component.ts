56088f043a069d9a1e71e6eda5aef7a5ec2deb0a
vmao19 <victormao98@gmail.com>
2020-09-02 23:11:32 -0400

    Add tabs for info about teams

--- src/app/about-page/about-page.component.ts
+++ src/app/about-page/about-page.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FAQ } from './faq';
 
 @Component({
   selector: 'app-about-page',
   templateUrl: './about-page.component.html',
-  styleUrls: ['./about-page.component.css']
+  styleUrls: ['./about-page.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AboutPageComponent implements OnInit {
   faqs = new FAQ().data;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { BackgroundCometsComponent } from './background-comets/background-comets
 import {NavbarComponent} from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatExpansionModule} from '@angular/material/expansion';
+import { MatTabsModule } from '@angular/material/tabs'
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 
@@ -38,7 +39,8 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
       dataEncapsulation: false
     }),
     BrowserAnimationsModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatTabsModule
   ],
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
     providers: [],
