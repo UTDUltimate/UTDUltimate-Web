79fba8cc18354b53437efc37090640c483bda8f3
vmao19 <victormao98@gmail.com>
2020-09-28 21:10:20 -0400

    Revert "Merge pull request #25 from UTDUltimate/develop"
    This reverts commit a0b6d05a9d6f67b2fa5821b1b28a08a493ac7dbd, reversing
    changes made to 6b8ae36657f9701e228b4278c69f44867ad50397.

--- src/app/Home/home.component.ts
+++ src/app/Home/home.component.ts
@@ -1,21 +1,15 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
-
-declare var twttr: any;
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit, AfterViewInit {
+export class HomeComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
-  }
-
-  ngAfterViewInit () {
-    twttr.widgets.load();
   }
 
 }
