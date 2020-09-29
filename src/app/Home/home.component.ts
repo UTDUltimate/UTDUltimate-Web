5d252b11f0370869dbed56413a2780c2c681948a
vmao19 <victormao98@gmail.com>
2020-09-28 21:37:25 -0400

    Revert "Merge branch 'master' into develop-temp"
    This reverts commit 6f662b1bf54e8c28cba19c715d12088cbeb35a82, reversing
    changes made to 9107a480a3974381b1a210d0d7817ad65d18bae1.

--- src/app/Home/home.component.ts
+++ src/app/Home/home.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+declare var twttr: any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit () {
+    twttr.widgets.load();
   }
 
 }
